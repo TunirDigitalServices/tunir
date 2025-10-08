b536195ae44ec4a4b1b16cb5d64927ed44f5eb09
Ranya ELOUNI <raniaelounidev@gmail.com>
2025-10-08 20:44:35 +0100

    add sharp libr and make unoptimized: true

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,10 +1,10 @@
 import Link from 'next/link';
 import Image from 'next/image';
-// import logoUni from '../images/logounivers.png';
+import logoUni from '../images/logounivers.png';
 const Logo = () => {
   return (
     <Link href='/'>
-      {/* <Image src={logoUni} width={120} height={"100%"} alt='logo univers' /> */}
+      <Image src={logoUni} width={120} height={"100%"} alt='logo univers' />
     </Link>
   );
 };
