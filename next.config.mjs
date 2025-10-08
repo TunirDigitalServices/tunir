dc35967eb27199a60d6cc9f9b54f6183297a7bb6
Ranya ELOUNI <raniaelounidev@gmail.com>
2025-10-08 20:20:29 +0100

    add sharp libr and make unoptimized: true

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'encrypted-tbn0.gstatic.com',
-          },
-          {
-            protocol: 'https',
-            hostname: 'images.unsplash.com',
-          },
-          {
-            protocol: 'https',
-            hostname: 'assets.aceternity.com',
-          },
-        ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "encrypted-tbn0.gstatic.com",
       },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "assets.aceternity.com",
+      },
+    ],
+     
+    unoptimized: true, // disables Next.js image optimization
+  
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
