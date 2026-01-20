2cd2eeedbfb1abd6ee3624a9f5f6347a8935f820
Ranya ELOUNI <raniaelounidev@gmail.com>
2026-01-20 15:48:37 +0100

    test 2

--- src/app/(routes)/sfectonir/page.jsx
+++ src/app/(routes)/sfectonir/page.jsx
@@ -17,51 +17,8 @@ export default function page() {
 
   return (
     <>
-      <HeroSfect />
-      <AboutSfect />
-      <Labs />
-      <KeyAchievement />
-      {/* <Partners/> */}
-      <Gallery />
-      <div id="pre-register-form">
-          <section className="mx-auto xl:container md:container sm:container py-8 lg:py-24 px-4">
-       
-         <Heading
-          tit1="Join Our "
-          tit2={"Upcoming Program"}
-          // tit2="Pre-register today and be the first to experience innovation, collaboration, and growth."
-        />
-        <FormSfect />
-       
-      </section>
-      </div>
-        <section className="mx-auto xl:container md:container sm:container py-8 lg:py-24 px-4">
-      <div className="text-center mb-12">
-        <Heading tit1="Testimonials" tit2="What They Say About Us" />
-
-        {/* Tabs */}
-        <Tabs defaultValue="students">
-          <div className="flex justify-center my-8">
-            <TabsList className="border rounded-xl w-full max-w-[400px]">
-              <TabsTrigger value="students" className="w-1/2">
-                Students
-              </TabsTrigger>
-              <TabsTrigger value="professionals" className="w-1/2">
-                Professionals
-              </TabsTrigger>
-            </TabsList>
-          </div>
-
-          {/* Tab Contents */}
-          <TabsContent value="students">
-            <Testimonials />
-          </TabsContent>
-          <TabsContent value="professionals">
-            <ProfessionalTestimonials />
-          </TabsContent>
-        </Tabs>
-      </div>
-    </section>
+hello
+ 
     </>
   );
 }
