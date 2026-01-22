07295006b5a88cc63752495e06a4462c633f756a
Ranya ELOUNI <raniaelounidev@gmail.com>
2026-01-22 16:34:17 +0100

    adding prisma post contact feature

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -4,17 +4,15 @@ import { PrismaPg } from "@prisma/adapter-pg";
 
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
-const prisma =
+// Use `new PrismaPg()` here
+const prismaAdapter = new PrismaPg({
+  url: process.env.DATABASE_URL!, // make sure this exists
+});
+
+export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    adapter: new PrismaPg({
-      connectionString: process.env.DATABASE_URL!, 
-    }),
-    log: ["query", "warn", "error"], 
+    adapter: prismaAdapter,
   });
 
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
-}
-
-export default prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
