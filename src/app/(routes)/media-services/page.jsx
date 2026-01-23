f4b825ab440dfdb04ca212b1079ecab7589bd71c
Ranya ELOUNI <raniaelounidev@gmail.com>
2026-01-23 17:25:50 +0100

    fix responsivness

--- src/app/(routes)/media-services/page.jsx
+++ src/app/(routes)/media-services/page.jsx
@@ -12,13 +12,13 @@ export default function page() {
       <div className="relative">
       <Podcast />
       <MarketingStra />
-      <div
+      {/* <div
         aria-hidden="true"
         class="md:flex absolute top-80 start-3/4 z-20 transform -translate-x-1/2 hidden "
       >
         <div class="bg-gradient-to-tl from-orange-100 via-orange-200 to-orange-100 blur-3xl md:w-[55rem] h-[35rem] rounded-full origin-top-left -rotate-12 -translate-x-[15rem] dark:from-orange-900/20 dark:via-orange-900/70 dark:to-orange-900/70"></div>
         <div class="bg-gradient-to-r from-[#50e2d4]/20 to-purple-100 blur-3xl md:w-[95rem] h-[20rem] rotate-[-60deg] transform -translate-x-[10rem] dark:from-violet-900/50 dark:to-purple-900"></div>
-      </div>
+      </div> */}
       </div>
     </div>
   );
