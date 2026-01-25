c4d6b953e9374917fc7c451297ef36d00cfc48cf
Ranya ELOUNI <raniaelounidev@gmail.com>
2026-01-25 19:11:19 +0100

    test on ipho

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -6,23 +6,23 @@ import "aos/dist/aos.css";
 // components
 import Logo from "../components/Logo";
 // import ThemeToggler from './ThemeToggler';
-// import Nav from "../components/Nav";
+import Nav from "../components/Nav";
 import MobileNav from "../components/MobileNav";
 import { usePathname } from "next/navigation";
 const Header = () => {
   const [header, setHeader] = useState(false);
   const pathname = usePathname();
 
-  // useEffect(() => {
-  //   const handleScroll = () => {
-  //     window.scrollY > 50 ? setHeader(true) : setHeader(false);
-  //   };
+  useEffect(() => {
+    const handleScroll = () => {
+      window.scrollY > 50 ? setHeader(true) : setHeader(false);
+    };
 
-  //   window.addEventListener("scroll", handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
-  //   // Cleanup event listener on component unmount
-  //   return () => window.removeEventListener("scroll", handleScroll);
-  // }, []);
+    // Cleanup event listener on component unmount
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
     <div
@@ -38,11 +38,11 @@ const Header = () => {
             <Logo />
             <div className="flex items-center gap-x-6">
               {/* nav */}
-              {/* <Nav
+              <Nav
                 containerStyles="hidden xl:flex gap-x-8 items-center"
                 linkStyles="relative hover:text-primary transition-all"
                 underlineStyles="absolute left-0 top-full h-[2px] bg-primary w-full"
-              /> */}
+              />
               {/* <ThemeToggler /> */}
               {/* mobile nav */}
               <div className="xl:hidden">
