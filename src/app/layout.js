cf23841b98d8a27c39d31dbb4d781ad101f08f3a
Ranya ELOUNI <raniaelounidev@gmail.com>
2026-01-25 18:50:12 +0100

    test on ipho

--- src/app/layout.js
+++ src/app/layout.js
@@ -36,7 +36,7 @@ export default function RootLayout({
       >
         {/* <Header /> */}
         <main>{children}</main>
-        {/* <Footer /> */}
+        <Footer />
       </body>
     </html>
   );
