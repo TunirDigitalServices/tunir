fed5866c685f3c879fa90f39f5faada6f3c1109b
Ranya ELOUNI <raniaelounidev@gmail.com>
2026-01-25 17:52:37 +0100

    test on ipho

--- src/app/template.jsx
+++ src/app/template.jsx
@@ -1,12 +1,9 @@
 'use client';
 
 import { motion } from 'framer-motion';
-
-// hooks
-import useScrollProgress from '../hooks/useScrollProgress';
 import { useEffect } from 'react';
+import useScrollProgress from '../hooks/useScrollProgress';
 
-// variants
 const variants = {
   hidden: { opacity: 0 },
   enter: { opacity: 1 },
@@ -15,52 +12,53 @@ const variants = {
 const Template = ({ children }) => {
   const completion = useScrollProgress();
 
+  useEffect(() => {
+    window.onerror = function (message, source, lineno, colno, error) {
+      document.body.innerHTML = `
+        <pre style="
+          white-space: pre-wrap;
+          padding: 16px;
+          color: red;
+          font-size: 14px;
+        ">
+❌ JAVASCRIPT ERROR
 
-useEffect(() => {
-  window.onerror = function (message, source, lineno, colno, error) {
-    document.body.innerHTML = `
-      <pre style="white-space: pre-wrap; padding: 16px; color: red;">
-ERROR:
+Message:
 ${message}
 
-SOURCE:
+Source:
 ${source}:${lineno}:${colno}
 
-STACK:
+Stack:
 ${error?.stack}
-      </pre>
-    `;
-  };
+        </pre>
+      `;
+    };
 
-  window.addEventListener("unhandledrejection", (event) => {
-    document.body.innerHTML = `
-      <pre style="white-space: pre-wrap; padding: 16px; color: red;">
-PROMISE ERROR:
+    window.addEventListener('unhandledrejection', (event) => {
+      document.body.innerHTML = `
+        <pre style="
+          white-space: pre-wrap;
+          padding: 16px;
+          color: red;
+          font-size: 14px;
+        ">
+❌ PROMISE ERROR
+
 ${event.reason}
-      </pre>
-    `;
-  });
-}, []);
-
-
+        </pre>
+      `;
+    });
+  }, []);
 
   return (
-    <>
-      <motion.main
-        variants={variants}
-        initial='hidden'
-        animate='enter'
-        transition={{ type: 'linear', delay: 0.2, duration: 0.4 }}
-      >
-        {children}
-      </motion.main>
-      {/* completion bar */}
-      <span
-        style={{ transform: `translateY(${completion - 100}%)` }}
-        className='fixed z-50 bg-primary w-1.5 top-0 right-0 bottom-0 transition-all duration-700'
-      ></span>
-      {/* <div className='h-[4000px]'></div> */}
-    </>
+    <motion.div
+      variants={variants}
+      initial="hidden"
+      animate="enter"
+    >
+      {children}
+    </motion.div>
   );
 };
 
